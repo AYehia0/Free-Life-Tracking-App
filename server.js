d9e369fce0bd78046f688f4417a74b3e15ab6ce7
AYehia0 <ahmedyehia611@gmail.com>
2021-04-30 16:43:34 +0200

    server !!

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require("./app")
+const http = require("http");
+
+const PORT = 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT , () => {
+    console.log(`Server is running on: ${PORT}`);
+})
+
+
+
+
