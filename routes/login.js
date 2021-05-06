89353958abc90ecf7ec68f4105e236bfb277cb4f
AYehia0 <ahmedyehia611@gmail.com>
2021-05-06 13:40:35 +0200

    route for POST login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,18 @@
 // To handling login
 const express = require("express");
 const router  = express.Router();
+const passport = require("passport");
 
 router.get("/", (req, res, next)=> {
     res.render("../views/login");
 });
+
+router.post("/", (req, res, next) => {
+    passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/login",
+        failureFlash: true
+    })(req, res, next)
+})
 
 module.exports = router;
