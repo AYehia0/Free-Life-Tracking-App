12fc7e75e4874cd3a2ed4df70cabf5696234c2e0
AYehia0 <ahmedyehia611@gmail.com>
2021-05-07 00:22:22 +0200

    protecting the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,12 @@
 // showing the loss weight process + some charts etc...
 const express = require("express");
+const {ensureAuth} = require("../config/auth")
 const router  = express.Router();
 
-router.get("/", (req, res, next)=> {
-    res.status(200).json({"message" : "dashboard"})
+router.get("/" , ensureAuth, (req, res, next)=> {
+    res.render("../views/dashboard", {
+        username: req.user.name
+    })
 });
 
 module.exports = router;
