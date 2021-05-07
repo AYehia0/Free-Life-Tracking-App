5b3219e502cfc91893d099d31aa2e228a6fd4737
AYehia0 <ahmedyehia611@gmail.com>
2021-05-07 13:21:58 +0200

    saving the login status to the server side locals

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,7 @@
 module.exports = {
     ensureAuth: function (req, res, next) {
         if(req.isAuthenticated()){
+            res.locals.login = req.session.passport;
             return next()
         }else{
             req.flash("errorMsg", "Login First");
