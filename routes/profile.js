0f3c4d49355a29eb1fc27300545cd2c7bed774f1
AYehia0 <ahmedyehia611@gmail.com>
2021-05-10 03:05:27 +0200

    showing complete user profile + editing [WORKING]

--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,40 @@
 // route to handle : profile/edit .... to show[GET], edit[PUT]
 const express = require("express");
 const {ensureAuth} = require("../config/auth")
+const bodyThings = require("../models/bodySpecific")
 const router  = express.Router();
 
 //showing the profile page
+
 router.get("/", ensureAuth, (req, res, next)=> {
-    res.render("../views/profile", {
-        username: req.user.name,
-        user: req.user
+
+    bodyThings.find({user:req.user}).then(user => {
+        if (user) {
+            res.render("../views/profile", {
+                username: req.user.name,
+                user: req.user,
+                bodythings: user[0] 
+            })
+        }
+    }).catch(err => {
+        console.log(err)
     })
 });
 
+router.get("/edit", ensureAuth, (req, res, next) => {
+
+    res.render("../views/editProfile", {
+            user: req.user,
+            name: req.name,
+            
+        })
+})
+
+// router.post("/edit", ensureAuth, (req, res, next) => {
+//     // change user's info 
+//     // /profile/edit
+
+// })
 //editing the profile page
 router.patch("/:profileId", (req, res, next)=> {
     const profileID = req.params.profileId;
