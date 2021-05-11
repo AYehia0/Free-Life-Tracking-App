d5144b185a93e8eba4f9e9635626e3789d199ef4
AYehia0 <ahmedyehia611@gmail.com>
2021-05-11 16:30:21 +0200

    route for editing profile page

--- routes/profile.js
+++ routes/profile.js
@@ -3,8 +3,11 @@ const express = require("express");
 const {ensureAuth} = require("../config/auth")
 const bodyThings = require("../models/bodySpecific")
 const router  = express.Router();
+const User = require("../models/user")
+const Bodythings = require("../models/bodySpecific")
 
 //showing the profile page
+const VALID_PASSWORD_LEN = process.env.PASS_LEN
 
 router.get("/", ensureAuth, (req, res, next)=> {
 
@@ -25,17 +28,63 @@ router.get("/edit", ensureAuth, (req, res, next) => {
 
     res.render("../views/editProfile", {
             user: req.user,
-            name: req.name,
-            
+            username: req.user.name
         })
 })
 
-// router.post("/edit", ensureAuth, (req, res, next) => {
-//     // change user's info 
-//     // /profile/edit
+router.post("/edit", ensureAuth, (req, res, next) => {
 
-// })
-//editing the profile page
+    const {name, age, password1, password2, height, weight, activity} = req.body;
+
+    // SomeValidation blah blah
+    // a Goog practice is to make a function for validation and use it for register and edit ,,,  will do it later
+
+    // Checking for errors
+    let errors = [];
+
+    // Checking empty fields 
+    if (name || age || (password1 && password2) || height || weight || activity) {
+        if (password1 !== password2) {
+            errors.push({errMsg: "Passwords aren't the same!!!"});
+        }
+        if (password1 && password2){
+            if (password1.length < VALID_PASSWORD_LEN || password2.length < VALID_PASSWORD_LEN ){
+                errors.push({errMsg: "Check password length > 4"});
+            }
+        }
+        
+        if (errors.length > 0) {
+            // If there are errors re-render the page 
+            return res.render("../views/editProfile", {
+                errors,
+                name, age, password1, password2, height, weight, activity,
+                user: req.user,
+                username: req.user.name
+            }) 
+        } else {
+            // Update the database
+            // then redirect to profile page
+            User.findOneAndUpdate({email: req.user.email}, {
+                name: name,
+                password: password1
+            })
+
+
+        }       
+
+    }else{
+        errors.push({errMsg: "Change at least one thing"});
+        // If there are errors re-render the page 
+        return res.render("../views/editProfile", {
+            errors,
+            name, age, password1, password2, height, weight, activity,
+            user: req.user,
+            username: req.user.name
+        }) 
+    }
+
+})
+// editing the profile page
 router.patch("/:profileId", (req, res, next)=> {
     const profileID = req.params.profileId;
     res.status(200).json({"message" : `editing profile ${profileID}`})
